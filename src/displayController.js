1f4fbac61a9cdbd6bdb8b2ac8aa2d40862a786f1
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-15 14:33:37 +0700

    display project name to the dom

--- src/displayController.js
+++ src/displayController.js
@@ -1,22 +1,39 @@
 export function displayTodoToDOM(container, projectID) {
-  resetDisplay()
-  const projectDOM = document.querySelector('.content')
-  for (let todo in container[projectID]) {
+  resetTodoDisplay()
+  const todoContainer = document.querySelector('.todo-container')
+  todoContainer.classList.add('todo-container')
+  for (let todo in container[projectID].todo) {
     const list = document.createElement('ul')
-    for (let prop in container[projectID][todo]) {
+    for (let prop in container[projectID].todo[todo]) {
       const li = document.createElement('li')
-      li.innerText = `${prop} : ${container[projectID][todo][prop]}`
+      li.innerText = `${prop} : ${container[projectID].todo[todo][prop]}`
       list.append(li)
     }
-    projectDOM.append(list)
+    todoContainer.append(list)
   }
 }
 
-function resetDisplay() {
-  const content = document.querySelector('.content')
-  content.innerHTML = ''
+function resetTodoDisplay() {
+  const todoContainer = document.querySelector('.todo-container')
+  todoContainer.innerHTML = ''
 }
-
+function resetProjectDisplay() {
+  const projectContainer = document.querySelector('.project-container')
+  projectContainer.innerHTML = ''
+}
 export function selectDOM(selector) {
   return document.querySelector(selector)
+}
+
+export function displayProjectToDOM(container) {
+  resetProjectDisplay()
+  const projectContainer = document.querySelector('.project-container')
+  projectContainer.classList.add('project-container')
+  console.log(projectContainer)
+  for (let prop in container) {
+    const div = document.createElement('div')
+    div.innerText = container[prop]['name']
+    console.log(div)
+    projectContainer.append(div)
+  }
 }
