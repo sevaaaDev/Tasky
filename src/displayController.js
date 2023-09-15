78431c216a50a6738cb246a1cd66c8e538af645a
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-15 16:18:31 +0700

    change id to index

--- src/displayController.js
+++ src/displayController.js
@@ -1,39 +1,37 @@
-export function displayTodoToDOM(container, projectID) {
-  resetTodoDisplay()
-  const todoContainer = document.querySelector('.todo-container')
-  todoContainer.classList.add('todo-container')
-  for (let todo in container[projectID].todo) {
-    const list = document.createElement('ul')
-    for (let prop in container[projectID].todo[todo]) {
-      const li = document.createElement('li')
-      li.innerText = `${prop} : ${container[projectID].todo[todo][prop]}`
-      list.append(li)
+export function displayTodoToDOM(container, projectIndex) {
+  resetTodoDisplay();
+  const todoContainer = document.querySelector(".todo-container");
+  todoContainer.classList.add("todo-container");
+  for (let todo in container[projectIndex].todo) {
+    const list = document.createElement("ul");
+    for (let prop in container[projectIndex].todo[todo]) {
+      const li = document.createElement("li");
+      li.innerText = `${prop} : ${container[projectIndex].todo[todo][prop]}`;
+      list.append(li);
     }
-    todoContainer.append(list)
+    todoContainer.append(list);
   }
 }
 
 function resetTodoDisplay() {
-  const todoContainer = document.querySelector('.todo-container')
-  todoContainer.innerHTML = ''
-}
-function resetProjectDisplay() {
-  const projectContainer = document.querySelector('.project-container')
-  projectContainer.innerHTML = ''
-}
-export function selectDOM(selector) {
-  return document.querySelector(selector)
+  const todoContainer = document.querySelector(".todo-container");
+  todoContainer.innerHTML = "";
 }
 
-export function displayProjectToDOM(container) {
-  resetProjectDisplay()
-  const projectContainer = document.querySelector('.project-container')
-  projectContainer.classList.add('project-container')
-  console.log(projectContainer)
-  for (let prop in container) {
-    const div = document.createElement('div')
-    div.innerText = container[prop]['name']
-    console.log(div)
-    projectContainer.append(div)
-  }
+function resetProjectDisplay() {
+  const projectContainer = document.querySelector(".project-container");
+  projectContainer.innerHTML = "";
+}
+
+export function selectDOM(selector) {
+  return document.querySelector(selector);
+}
+
+export function displayProjectToDOM(container, projectIndex) {
+  const projectContainer = document.querySelector(".project-container");
+  const div = document.createElement("div");
+  div.innerText = container[projectIndex]["name"];
+  div.setAttribute("data-id", projectIndex);
+  div.classList.add("project");
+  projectContainer.append(div);
 }
