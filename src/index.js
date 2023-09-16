7ca5bdd4a53ffed08b863a246a3ad0eea8d7aec5
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-16 13:42:05 +0700

    implement local storage

--- src/index.js
+++ src/index.js
@@ -1,43 +1,59 @@
-import Container from "./todoCreator.js";
 import { createProject, createTodo } from "./todoCreator.js";
 import { checklist, editTodo, removeTodo } from "./todoEditor.js";
-import { displayTodoToDOM, selectDOM, displayProjectToDOM } from "./displayController.js";
+import {
+  displayTodoToDOM,
+  selectDOM,
+  displayProjectToDOM,
+} from "./displayController.js";
 
-let container = []
-let currentProject
-function createNewProject(name) {
-  let projectID = createProject(container, name)
-  displayProjectToDOM(container, container.length - 1)
-  let project = selectDOM(`[data-id='${container.length - 1 }'`)
-  project.addEventListener('click', () => {
-    if (currentProject === project.dataset.id) {
-      return
+function startScript() {
+  let container = [];
+  let currentProject = 0;
+  checkLocalStorage();
+  function checkLocalStorage() {
+    if (localStorage.length !== 0) {
+      container = JSON.parse(localStorage.getItem("container"));
+      console.log(container)
+      displayProjectToDOM(container, container.length - 1);
+      displayTodoToDOM(container, currentProject);
+      return;
     }
-    currentProject = project.dataset.id
-    displayTodoToDOM(container, currentProject)
-    console.log(currentProject)
-  })
+    createNewProject("js");
+  }
+
+  function createNewProject(name) {
+    createProject(container, name);
+    displayProjectToDOM(container, container.length - 1);
+    localStorage.setItem("container", JSON.stringify(container));
+    let project = selectDOM(`[data-id='${container.length - 1}'`);
+    project.addEventListener("click", () => {
+      if (currentProject === +project.dataset.id) return;
+      currentProject = +project.dataset.id;
+      displayTodoToDOM(container, currentProject);
+      console.log(currentProject);
+    });
+  }
+
+  const input = {
+    name: document.querySelector("#name"),
+    desc: document.querySelector("#desc"),
+    date: document.querySelector("#date"),
+    priority: document.querySelector("#priority"),
+  };
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createTodo(
+      container,
+      currentProject,
+      input.name.value,
+      input.desc.value,
+      input.date.value,
+      input.priority.value
+    );
+    displayTodoToDOM(container, currentProject);
+    localStorage.setItem("container", JSON.stringify(container));
+  });
 }
-createNewProject('js', 0)
-createNewProject('html', 1)
-createNewProject('css', 2)
-
-let projectDOM = document.getElementsByClassName('project')
-console.log(projectDOM)
-
-let key = Object.keys(container)
-console.log(container)
-
-const input = {
-  name: document.querySelector('#name'),
-  desc: document.querySelector('#desc'),
-  date: document.querySelector('#date'),
-  priority: document.querySelector('#priority'),
-}
-
-const form = document.querySelector('form')
-form.addEventListener('submit' ,(e) => {
-  e.preventDefault()
-  createTodo(container, currentProject, input.name.value, input.desc.value, input.date.value, input.priority.value)
-  displayTodoToDOM(container, currentProject)
-})
+startScript();
