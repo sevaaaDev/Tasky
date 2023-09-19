9f572c81748aad02c309770734c2d49363ad488e
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-19 16:06:18 +0700

    add function that manipulate navigation bar

--- src/displayController.js
+++ src/displayController.js
@@ -1,16 +1,13 @@
 export function displayTodoToDOM(container, projectIndex) {
   resetTodoDisplay();
-  const todoContainer = document.querySelector(".todo-container");
-  todoContainer.classList.add("todo-container");
+  const taskContainer = document.querySelector(".main--task-container");
   for (let todo of container[projectIndex].todo) {
-    const list = document.createElement("ul");
     for (let prop in todo) {
-      const li = document.createElement("li");
-      list.setAttribute("data-id", container[projectIndex].todo.indexOf(todo));
-      li.innerText = `${prop} : ${todo[prop]}`;
-      list.append(li);
+      const todoElement = document.createElement("div");
+      todoElement.setAttribute("data-id", todo["oriIndex"]);
+      todoElement.innerText = `${todo["name"]}`;
+      taskContainer.append(todoElement);
     }
-    todoContainer.append(list);
   }
 }
 
@@ -29,10 +26,56 @@ export function selectDOM(selector) {
 }
 
 export function displayProjectToDOM(container, projectIndex) {
-  const projectContainer = document.querySelector(".project-container");
+  const projectContainer = document.querySelector(".nav--project-container");
   const div = document.createElement("div");
-  div.innerText = container[projectIndex]["name"];
-  div.setAttribute("data-id", projectIndex);
+  const icon = document.createElement("img");
+  icon.setAttribute("src", "icons/folder.svg");
+  div.append(icon, container[projectIndex]["name"]);
+  div.setAttribute("data-index", projectIndex);
   div.classList.add("project");
   projectContainer.append(div);
+}
+
+export function highlightSelected(e) {
+  const btnFilter = document.querySelectorAll('nav [class*="button"]');
+  const btnProject = document.querySelectorAll(
+    'nav .nav--project-container [class*="project"]'
+  );
+  resetHighlighting(btnProject);
+  resetHighlighting(btnFilter);
+  if (e.target.className !== "project") {
+    for (let btn of btnFilter) {
+      if (e.target.className === btn.className) {
+        btn.classList.add("selected");
+      }
+    }
+    return;
+  }
+  console.log(btnProject);
+  e.target.classList.add("selected");
+}
+
+function resetHighlighting(nodelist) {
+  for (let elem of nodelist) {
+    elem.classList.remove("selected");
+  }
+}
+
+export function showFormProject(e) {
+  const form = document.querySelector('[class*="form--add-project"]');
+  const btnAdd = document.querySelector('[class*="button--add-project"]');
+  const input = form.querySelector("input");
+  form.classList.add("form--add-project");
+  btnAdd.classList.add("button--add-project__invisible");
+  btnAdd.classList.remove("button--add-project");
+  input.focus();
+}
+
+export function cancelAddProject() {
+  const form = document.querySelector('[class*="form--add-project"]');
+  const btnAdd = document.querySelector('[class*="button--add-project"]');
+  const input = form.querySelector("input");
+  input.value = "";
+  form.classList.toggle("form--add-project");
+  btnAdd.classList.toggle("button--add-project");
 }
