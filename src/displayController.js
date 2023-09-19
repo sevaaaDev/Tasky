37f743a21c2d70d21ca2b20697c70e3a79842a11
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-19 16:22:14 +0700

    now u can also open the add task form

--- src/displayController.js
+++ src/displayController.js
@@ -62,12 +62,24 @@ function resetHighlighting(nodelist) {
 }
 
 export function showFormProject(e) {
-  const form = document.querySelector('[class*="form--add-project"]');
-  const btnAdd = document.querySelector('[class*="button--add-project"]');
-  const input = form.querySelector("input");
-  form.classList.add("form--add-project");
-  btnAdd.classList.add("button--add-project__invisible");
-  btnAdd.classList.remove("button--add-project");
+  const btnAddProject = document.querySelector(
+    '[class*="button--add-project"]'
+  );
+  if (e.target === btnAddProject) {
+    const formProject = document.querySelector('[class*="form--add-project"]');
+    const input = formProject.querySelector("input");
+    formProject.classList.add("form--add-project");
+    btnAddProject.classList.add("button--add-project__invisible");
+    btnAddProject.classList.remove("button--add-project");
+    input.focus();
+    return;
+  }
+  const btnAddTask = document.querySelector('[class*="button--add-task"]');
+  const formTask = document.querySelector('[class*="form--add-task"]');
+  const input = formTask.querySelector("input");
+  formTask.classList.add("form--add-task");
+  btnAddTask.classList.add("button--add-task__invisible");
+  btnAddTask.classList.remove("button--add-task");
   input.focus();
 }
 
