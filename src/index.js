b31c199d35b9a610d3e1e7522af91a645657a009
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-20 12:56:18 +0700

    fix task form issue

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   displayProjectToDOM,
   highlightSelected,
   showFormProject,
-  cancelAddProject,
+  cancelAdd,
 } from "./displayController.js";
 import { filterToday, filter7days } from "./todoFilter.js";
 
@@ -17,18 +17,25 @@ function addGlobalEventListener(type, selector, callback) {
     }
   });
 }
-addGlobalEventListener("click", ".button--all-tasks", highlightSelected);
-addGlobalEventListener("click", ".button--today-tasks", highlightSelected);
-addGlobalEventListener("click", ".button--thisweek-tasks", highlightSelected);
+addGlobalEventListener("click", ".button--all-tasks", selected);
+addGlobalEventListener("click", ".button--today-tasks", selected);
+addGlobalEventListener("click", ".button--thisweek-tasks", selected);
 addGlobalEventListener("click", ".button--add-project", showFormProject);
-addGlobalEventListener("click", "nav .button--x", cancelAddProject);
-addGlobalEventListener("click", "nav .button--x svg", cancelAddProject);
+addGlobalEventListener("click", "nav .button--x", cancelAdd.bind(null, 'project'));
+addGlobalEventListener("click", "nav .button--x svg", cancelAdd.bind(null, 'project'));
 addGlobalEventListener("click", "nav .button--check", formProject);
 addGlobalEventListener("click", "nav .button--check svg", formProject);
 addGlobalEventListener("submit", "nav form", formProject);
+addGlobalEventListener("click", ".button--add-task", showFormProject);
+addGlobalEventListener("click", "main .button--check", formTask);
+addGlobalEventListener("click", "main .button--check svg", formTask);
+addGlobalEventListener("submit", "main form", formTask);
+addGlobalEventListener("click", "main .button--x", cancelAdd.bind(null, 'task'));
+addGlobalEventListener("click", "main .button--x svg", cancelAdd.bind(null, 'task'));
 
 let container = [];
 let currentProject = 1;
+
 function createNewProject(container, name) {
   createProject(container, name);
   displayProjectToDOM(container, container.length - 1);
@@ -37,18 +44,34 @@ function createNewProject(container, name) {
   );
   console.log(project);
   project.addEventListener("click", (e) => {
-    if (currentProject === +project.dataset.index) return;
-    currentProject = +project.dataset.index;
-    highlightSelected(e);
-    console.log(currentProject);
-  });
+    selected(e)
+  })
 }
 
 function formProject(e) {
   e.preventDefault();
   const input = document.querySelector("nav form input");
   createNewProject(container, input.value);
-  cancelAddProject();
+  cancelAdd('project');
+}
+
+function formTask(e) {
+  e.preventDefault();
+  const inputTitle = document.querySelector("main form input");
+  const inputDate = document.querySelector('main form input[type="date"]');
+  console.log(inputDate, inputTitle)
+  createTodo(container, currentProject, inputTitle.value, inputDate.value);
+  displayTodoToDOM(container, currentProject)
+  cancelAdd('task');
 }
 
 createNewProject(container, "ReactJS");
+
+function selected(e) {
+  if (currentProject === e.target.dataset.index) return;
+  currentProject = e.target.dataset.index;
+  highlightSelected(e);
+  console.log(currentProject);
+  cancelAdd('task')
+  displayTodoToDOM(container, currentProject);
+}
