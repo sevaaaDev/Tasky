66bbc4552455ed940a21cdf566db78918e2488cc
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-22 16:38:39 +0700

    Add: resetDisplay func for project container

--- src/displayController/displayReset.js
+++ src/displayController/displayReset.js
@@ -2,3 +2,8 @@ export function resetTaskDisplay() {
   const taskContainer = document.querySelector(".main--task-container");
   taskContainer.innerHTML = "";
 }
+
+export function resetProjectDisplay() {
+  const projectContainer = document.querySelector('.nav--project-container')
+  projectContainer.innerHTML = ''
+}
