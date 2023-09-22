071178977d60214bdc765a11a7b8fa904b3709b6
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-22 16:33:13 +0700

    Change: how project are added to the dom

--- src/displayController/displayProject.js
+++ src/displayController/displayProject.js
@@ -1,10 +1,15 @@
+import { resetProjectDisplay } from "./displayReset";
+
 export function displayProjectToDOM(container, projectIndex) {
+  resetProjectDisplay()
   const projectContainer = document.querySelector(".nav--project-container");
-  const div = document.createElement("div");
-  const icon = document.createElement("img");
-  icon.setAttribute("src", "icons/folder.svg");
-  div.append(icon, container[projectIndex]["name"]);
-  div.setAttribute("data-index", projectIndex);
-  div.classList.add("project");
-  projectContainer.append(div);
+  for (let project of container) {
+    const div = document.createElement("div");
+    const icon = document.createElement("img");
+    icon.setAttribute("src", "icons/folder.svg");
+    div.append(icon, project["name"]);
+    div.setAttribute("data-index", project["ID"]);
+    div.classList.add("project");
+    projectContainer.append(div);
+  }
 }
