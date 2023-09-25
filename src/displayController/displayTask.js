9b40e7d3bfd2b8d5bfdbb00f9850d743ad33d445
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-25 13:39:54 +0700

    Add: func that display filtered task to dom

--- src/displayController/displayTask.js
+++ src/displayController/displayTask.js
@@ -1,6 +1,7 @@
 import { format } from "date-fns";
 import { resetTaskDisplay } from "./displayReset";
 import { disableAddTask, enableAddTask } from "./displayBehavior";
+import { filterToday, filter7days } from "../taskFilter";
 
 export function displayTaskToDOM(container, projectIndex) {
   const buttons = [{
@@ -15,6 +16,11 @@ export function displayTaskToDOM(container, projectIndex) {
   },]
   if (projectIndex === 'All Tasks') {
     displayAllTask(container, buttons)
+    disableAddTask()
+    return
+  }
+  if (projectIndex ===  'Today Tasks' || projectIndex === "This Week Tasks") {
+    displayFilteredTask(container, buttons, projectIndex)
     disableAddTask()
     return
   }
@@ -62,6 +68,50 @@ function displayAllTask(container, buttons) {
   const taskContainer = document.querySelector('.main--task-container')
   for(let project of container) {
     for(let task of project.task) {
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+    for (let button of buttons) {
+      const btn = document.createElement('button') 
+      btn.setAttribute('type', 'button')
+      btn.classList.add(`button--${button.type}`)
+      const img = document.createElement('img')
+      img.setAttribute('src', `icons/${button.icon}`)
+      btn.append(img)
+      taskElement.append(btn)
+    }
+    const infoWrapper = document.createElement('div')
+    infoWrapper.classList.add('task--info-wrapper')
+    const title = document.createElement('p')
+    title.innerText = task.name
+    title.classList.add('task--title')
+    const dueDate = document.createElement('p')
+    dueDate.innerText = `Due ${format(task.dueDate, 'd MMM yyyy')}`
+    dueDate.classList.add('task--date')
+    taskElement.setAttribute("data-index", task["ID"]);
+    taskElement.setAttribute("data-project", task["project"]);
+    taskContainer.append(taskElement);
+    const btnEdit = document.querySelector(`main div[data-project="${task.project}"][data-index="${task.ID}"] .button--edit`)
+    const btnStatusIcon = document.querySelector(`main div[data-project="${task.project}"][data-index="${task.ID}"] .button--status img`)
+    if (task.checklist === true) {
+      title.classList.add('checked')
+      dueDate.classList.add('checked')
+      btnStatusIcon.setAttribute('src', 'icons/check-circle.svg')
+    }
+    infoWrapper.append(title, dueDate)
+    taskElement.insertBefore(infoWrapper, btnEdit) 
+    }
+  }
+}
+
+function displayFilteredTask(container, buttons, projectIndex) {
+  resetTaskDisplay()
+  let filtered = filter7days(container) 
+  if (projectIndex === "Today Tasks") {
+    filtered = filterToday(container)
+  }
+  const taskContainer = document.querySelector('.main--task-container')
+  for(let project of filtered) {
+    for(let task of project) {
     const taskElement = document.createElement("div");
     taskElement.classList.add("task");
     for (let button of buttons) {
