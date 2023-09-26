b32d8420ef2dcb235e50dca136edb5df7fd8084f
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-26 18:44:07 +0700

    Add: display menu
    to display more option for editing task

---
+++ src/displayController/displayMenu.js
@@ -0,0 +1,36 @@
+import { displayTaskToDOM } from "./displayTask"
+
+export function displayMenuToDOM(menuContainer, container, projectIndex) {
+  if (document.querySelector('.menu') !== null) {
+    const currentMenu = document.querySelector('.menu')
+    if (currentMenu.closest('.menu--container') === menuContainer) {
+      hideMenu()
+      return
+    }
+    hideMenu()
+  }
+  const menu = document.createElement('div')
+  let className = [{
+    clas: 'menu--details',
+    text: "Details"
+  },{
+    clas: 'menu--edit',
+    text: "Edit"
+  },{
+    clas: 'menu--delete',
+    text: "Delete"
+  }] 
+  for (let elem of className) {
+    const btn = document.createElement('p')
+    btn.classList.add(elem.clas)
+    btn.innerText = elem.text
+    menu.append(btn)
+  }
+  menu.classList.add('menu')
+  menuContainer.append(menu);
+}
+
+export function hideMenu(container, projectIndex) {
+  const menu = document.querySelector('.menu')
+  menu.remove()
+}
