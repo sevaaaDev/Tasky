50db0983fe296684742d4ce015701f1fb15c7b0c
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-10-01 13:30:38 +0700

    Remove: inline source map to minimize bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
