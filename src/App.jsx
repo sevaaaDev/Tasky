d65b5ac303c861ed414c2c8dbc4c02a46f0d5140
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-09-13 12:48:55 +0700

    feat: simple prototype crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { Form } from "./components/Form";
 import "./App.css";
+import { GroupItem } from "./components/GroupItem";
 
 const initItems = {
+  Default: [],
   Programming: [
     {
       id: crypto.randomUUID(),
@@ -10,28 +15,61 @@ const initItems = {
       summary: "all hooks",
     },
     {
+      id: crypto.randomUUID(),
       title: "Learn React Router",
       summary: "router",
     },
   ],
+  Life: [],
 };
 function App() {
   const [items, setItems] = useState(initItems);
   const [currentGroup, setCurrentGroup] = useState("Programming"); // change it to default
   let currentItems = items[currentGroup];
+  let listOfGroups = Object.keys(items);
+
+  function deleteGroupCurry(id) {
+    return () => {
+      let { [id]: _, ...newItems } = items;
+      setCurrentGroup("Default");
+      setItems(newItems);
+    };
+  }
+
+  function deleteTodoCurry(id) {
+    return () => {
+      let newItems = { ...items };
+      newItems[currentGroup] = newItems[currentGroup].filter(
+        (el) => el.id !== id,
+      );
+      setItems(newItems);
+    };
+  }
+
+  function addTodo(title) {
+    let newItems = { ...items };
+    newItems[currentGroup].push({
+      id: crypto.randomUUID(),
+      title: title,
+      summary: "",
+    });
+    setItems(newItems);
+  }
   return (
     <>
-      <TodoList items={currentItems} />
+      <Form submit={addTodo} label="Todo"></Form>
+      <TodoList>
+        {currentItems.map((el) => (
+          <TodoItem key={el.id} item={el} handleDeleteCurry={deleteTodoCurry} />
+        ))}
+      </TodoList>
+      <hr />
+      <TodoList>
+        {listOfGroups.map((el) => (
+          <GroupItem key={el} group={el} handleDeleteCurry={deleteGroupCurry} />
+        ))}
+      </TodoList>
     </>
-  );
-}
-function TodoList({ items }) {
-  return (
-    <ul className="Todo-List">
-      {items.map((el) => (
-        <li key={el.id}>{el.title}</li>
-      ))}
-    </ul>
   );
 }
 
